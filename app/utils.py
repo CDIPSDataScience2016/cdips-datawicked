454cdff9236c90b30ccbf24e81ff61f7ed6214b7
mike-a-yen <mike.a.yen@gmail.com>
2016-07-24 14:24:51 -0700

    config and utils

---
+++ app/utils.py
@@ -0,0 +1,9 @@
+class bcolors:
+    header = '\033[95m'
+    blue = '\033[94m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    endc = '\033[0m'
+    cold = '\033[1m'
+    underline = '\033[4m'
