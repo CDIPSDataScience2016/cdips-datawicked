39ab8f96d1218d185a1e790e7f6f33b84d15ee6f
Ubuntu <ubuntu@ip-172-31-19-91.us-west-2.compute.internal>
2016-07-24 22:09:47 +0000

    new endpoint

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from .utils import bcolors
 import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 import json
+
+plotly.tools.set_credentials_file(username='mike-a-yen', api_key='7ijqoy41kr')
 
 
 @app.route('/')
@@ -19,12 +23,22 @@ def index():
                            posts=posts)
 
 
+@app.route('/product')
+def product():
+    #TODO Use bethans function to change parameters
+    # and return json of plotly
+    #plot_json = bethans_function(product,sentiment)
+    fi = open('app/static/Samsung_chromebook_sentiment.json', 'r')
+    graph = json.loads(fi.read())
+    data = graph['data']
+    link = py.iplot(data,filename).embed_code
+    return render_template('product_views.html',post=link)
+
 @app.route('/samsung')
 def samsung():
     fi = open('app/static/Samsung_chromebook_sentiment.json', 'r')
     graph = json.loads(fi.read())
     print(bcolors.green, 'Pre', graph, bcolors.endc)
-    graph = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
     print(bcolors.blue, 'Post', graph, bcolors.endc)
     return render_template('plots.html',
                            ids=['Samsung'],
