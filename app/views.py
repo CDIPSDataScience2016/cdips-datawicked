58b94978bf6199198cb498940176d1a14dde1d49
mike-a-yen <mike.a.yen@gmail.com>
2016-07-24 13:52:41 -0700

    about to push to aws

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
+from .utils import bcolors
 import plotly
+import json
 
 
 @app.route('/')
@@ -11,7 +13,24 @@ def index():
               'body': 'Hello!'},
              {'author': 'Bethan',
               'body': 'Goodbye!'}]
-    return render_template('plots.html',
+    return render_template('index.html',
                            title='CDIPS Data Wicked',
                            user=user,
                            posts=posts)
+
+
+@app.route('/samsung')
+def samsung():
+    fi = open('app/static/Samsung_chromebook_sentiment.json', 'r')
+    graph = json.loads(fi.read())
+    print(bcolors.green, 'Pre', graph, bcolors.endc)
+    graph = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
+    print(bcolors.blue, 'Post', graph, bcolors.endc)
+    return render_template('plots.html',
+                           ids=['Samsung'],
+                           graphJSON=[graph])
+
+
+@app.route('/product/<int:product_id>')
+def product():
+    return '404'
