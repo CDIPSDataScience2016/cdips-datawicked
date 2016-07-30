aad7a561a04340533d32665a24457db39e8c19b0
mike-a-yen <mike.a.yen@gmail.com>
2016-07-29 20:43:21 -0700

    new init

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,10 @@ import os
 home = os.getcwd()
 all_reviews = pd.read_csv(os.path.join(home,"data/top_10_electronics_reviews.csv"), sep='\t')
 all_phrases = pd.read_pickle(os.path.join(home,"app/static/product_sentiment_phrases.pkl"))
+
+df_review = pd.read_csv(os.path.join(home,"data/review-topic.csv"))
+df_topic = pd.read_csv(os.path.join(home,"data/topic-words-reviews.csv"))
+
 app = Flask(__name__)
 # app.config.from_object('config')
 
