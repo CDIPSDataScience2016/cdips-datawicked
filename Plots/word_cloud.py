c7d17f297d4c04c02ea927821eb9f6d2a3c97d11
mike-a-yen <mike.a.yen@gmail.com>
2016-07-29 18:02:19 -0700

    return cloud names

--- Plots/word_cloud.py
+++ Plots/word_cloud.py
@@ -46,6 +46,9 @@ def neg_color_func(word, font_size, position, orientation, random_state=None, **
 
 # Make word clouds from product string or list of product strings
 def make_word_cloud(products):
+    positve_name = '../app/static/img/pos_wordcloud.png'
+    negative_name = '../app/static/img/neg_wordcloud.png'
+
     if type(products) == str:
         new_pos_phrases, new_neg_phrases = get_phrases(products)
         freq_pos = get_word_freq(new_pos_phrases)
@@ -68,15 +71,16 @@ def make_word_cloud(products):
         wc = WordCloud(background_color="white", max_words=2000,
                max_font_size=300, random_state=42)
 
+
         # generate word cloud for positive
         wc.generate_from_frequencies(pos_words_array)
         wc.recolor(color_func=pos_color_func, random_state=3)
-        wc.to_file("../app/static/img/pos_wordcloud.png")
+        wc.to_file(positive_name)
 
         # generate word cloud for negative
         wc.generate_from_frequencies(neg_words_array)
         wc.recolor(color_func=neg_color_func, random_state=3)
-        wc.to_file("../app/static/img/neg_wordcloud.png")
+        wc.to_file(negative_name)
 
     elif type(products) == list:
         list_pos = []
@@ -110,13 +114,13 @@ def make_word_cloud(products):
                max_font_size=300, random_state=42)
 
         # generate word cloud for positive
-        positive_name = '../app/static/img/pos_wordcloud.png'
         wc.generate_from_frequencies(pos_words_array)
         wc.recolor(color_func=pos_color_func, random_state=3)
         wc.to_file(positive_name)
 
         # generate word cloud for negative
-        negative_name = '../app/static/img/neg_wordcloud.png'
         wc.generate_from_frequencies(neg_words_array)
         wc.recolor(color_func=neg_color_func, random_state=3)
-        wc.to_file(negative_name)        
+        wc.to_file(negative_name)
+
+    return positive_name, negative_name
